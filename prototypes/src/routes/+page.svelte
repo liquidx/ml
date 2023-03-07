6c3b883b2e35b861fee5c500269b95e91d53c7f8
Alastair Tse <alastair@liquidx.net>
2023-03-07 09:04:22 +0900

    Barebones impl of a ml prototype

--- prototypes/src/routes/+page.svelte
+++ prototypes/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte';
+	import { Input, Label, Helper } from 'flowbite-svelte';
+
+	let server = 'http://localhost:11000/predict';
+	let output: HTMLDivElement | null = null;
+
+	const onEnter = async (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			let requestUrl = new URL(server);
+			requestUrl.searchParams.append('prompt', e.target.value);
+			const response = await fetch(requestUrl.toString()).then((response) => response.json());
+			if (output && response && response.output) {
+				output.innerHTML = response.output;
+			}
+			console.log(response);
+		}
+	};
+</script>
+
+<div class="p-8">
+	Explain to me...
+	<Input placeholder="World Cup" on:keyup={onEnter} />
+	<div bind:this={output} />
+</div>
