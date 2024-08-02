be864605eaf6e65574ef508ea688f7ad99c808c7
Alastair Tse <alastair@liquidx.net>
2024-08-02 22:24:33 +0900

    Switch to digitalocean mongodb

--- prototypes/src/lib/mongodb.ts
+++ prototypes/src/lib/mongodb.ts
@@ -1,16 +1,14 @@
 import { MongoClient } from 'mongodb';
-import { MONGO_API_PASS } from '$env/static/private';
-import { MONGO_API_USER } from '$env/static/private';
+import { MONGO_DB_PASS } from '$env/static/private';
+import { MONGO_DB_USER } from '$env/static/private';
 
-export const MONGO_SERVER = 'cluster0.uakqn3b.mongodb.net'; // liquidx-projects
-export const MONGO_CLUSTER = 'Cluster0';
-
-const MONGO_SNOWLINE_SERVER = 'maincluster.ajbtgw2.mongodb.net';
-const MONGO_SNOWLINE_CLUSTER = 'MainCluster';
+export const MONGO_SERVER = 'db-mongodb-sfo2-liquidx-bdf6d203.mongo.ondigitalocean.com';
+export const MONGO_DB_NAME = 'ml';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const executeMongoQueries = async (body: (client: MongoClient) => Promise<any>) => {
-	const mongodbUrl = `mongodb+srv://${MONGO_API_USER}:${MONGO_API_PASS}@${MONGO_SERVER}/?retryWrites=true&w=majority&appName=${MONGO_CLUSTER}`;
+	const mongoDbParams = 'tls=true&authSource=admin&replicaSet=db-mongodb-sfo2-liquidx';
+	const mongodbUrl = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASS}@${MONGO_SERVER}/${MONGO_DB_NAME}?${mongoDbParams}`;
 
 	let client: MongoClient | undefined;
 	try {
